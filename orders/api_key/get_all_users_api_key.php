099b6ec8df7a8e5c64e5e8718ee378c911aec639
letambari <innocentdestiny228@gmail.com>
2023-05-29 21:42:44 +0100

    updating the orders database

--- orders/api_key/get_all_users_api_key.php
+++ orders/api_key/get_all_users_api_key.php
@@ -8,7 +8,6 @@ $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
 $database = $mongoClient->selectDatabase("storage_data");
 $collection = $database->selectCollection("orders");
 
-
 // Get the user's API key from the request headers
 if (!isset($_SERVER['HTTP_X_API_KEY'])) {
     // Return error for missing API key
@@ -33,22 +32,69 @@ if (!$access_document) {
     exit();
 }
 
+// Check if the permission field exists and is equal to 1
+if (isset($access_document['permission']) && ($access_document['permission'] === 1 || $access_document['permission'] === 2 || $access_document['permission'] === 3 || $access_document['permission'] === 4)) {
+    // User has full access
 
-// Define the API endpoint
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Get all documents from the collection
-    $cursor = $collection->find();
-    $documents = array();
-    foreach ($cursor as $document) {
-        $documents[] = $document;
+    // Validate and sanitize the page parameter
+    $page = isset($_GET['page']) ? filter_var($_GET['page'], FILTER_VALIDATE_INT) : 1;
+    if ($page === false || $page < 1) {
+        // Return error for invalid page value
+        header('HTTP/1.1 400 Bad Request');
+        header('Content-Type: application/json');
+        echo json_encode(array('error' => 'Invalid page value'));
+        exit();
     }
-    // Output the documents as JSON
-    header('Content-Type: application/json');
-    echo json_encode($documents);
+
+    // Validate and sanitize the limit parameter
+    $limit = isset($_GET['limit']) ? filter_var($_GET['limit'], FILTER_VALIDATE_INT) : 10;
+    if ($limit === false || $limit < 1) {
+        // Return error for invalid limit value
+        header('HTTP/1.1 400 Bad Request');
+        header('Content-Type: application/json');
+        echo json_encode(array('error' => 'Invalid limit value'));
+        exit();
+    }
+
+    // Calculate skip value for pagination
+    $skip = ($page - 1) * $limit;
+
+    // Define the API endpoint
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // Get total document count for pagination
+        $totalDocuments = $collection->countDocuments();
+
+        // Get paginated documents from the collection
+        $cursor = $collection->find([], ['limit' => $limit, 'skip' => $skip]);
+        $documents = iterator_to_array($cursor);
+
+        // Prepare pagination metadata
+        $pagination = array(
+            'totalDocuments' => $totalDocuments,
+            'currentPage' => $page,
+            'perPage' => $limit,
+            'totalPages' => ceil($totalDocuments / $limit)
+        );
+
+        // Output the documents and pagination metadata as JSON
+        $response = array(
+            'documents' => $documents,
+            'pagination' => $pagination
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        // Return error for unsupported HTTP method
+        header('HTTP/1.1 405 Method Not Allowed');
+        header('Content-Type: application/json');
+        echo json_encode(array('error' => 'Unsupported HTTP method'));
+    }
 } else {
-    // Return error for unsupported HTTP method
-    header('HTTP/1.1 405 Method Not Allowed');
+    // User does not have full access
+    header('HTTP/1.1 401 Unauthorized');
     header('Content-Type: application/json');
-    echo json_encode(array('error' => 'Unsupported HTTP method'));
+    echo json_encode(array('error' => 'Unauthorized access'));
+    exit();
 }
 ?>
--- orders/api_key/insert_user_with_api_key.php
+++ orders/api_key/insert_user_with_api_key.php
@@ -10,50 +10,208 @@ header("Access-Control-Allow-Headers: *");
 $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
 
 // Select the database and collection for user access
-$db_access = $mongoClient->selectDatabase("storage_data");
-$collection_access = $db_access->selectCollection("user_access");
+$dbAccess = $mongoClient->selectDatabase("storage_data");
+$collectionAccess = $dbAccess->selectCollection("user_access");
 
 // Check for API key
-if (!isset($_SERVER['HTTP_X_API_KEY'])) {
+$apiKey = $_SERVER['HTTP_X_API_KEY'] ?? null;
+
+if (!$apiKey) {
     // Return error for missing API key
-    header('HTTP/1.1 400 Bad Request');
-    header('Content-Type: application/json');
-    echo json_encode(array('error' => 'Missing API key'));
-    exit();
+    returnError('Missing API key', 400);
 }
 
 // Find the document with the given API key
-$filter = ['api_key' => $_SERVER['HTTP_X_API_KEY']];
-$document_access = $collection_access->findOne($filter);
+$accessFilter = ['api_key' => $apiKey];
+$accessDocument = $collectionAccess->findOne($accessFilter);
 
-if (!$document_access) {
+if (!$accessDocument) {
     // Return error for unauthorized access
-    header('HTTP/1.1 401 Unauthorized');
-    header('Content-Type: application/json');
-    echo json_encode(array('error' => 'Unauthorized access'));
-    exit();
+    returnError('Unauthorized access', 401);
+}
+
+// Check if the permission field exists and is equal to 1 or 2
+$permission = $accessDocument['permission'] ?? null;
+
+if (!in_array($permission, [1, 2])) {
+    // User does not have full access
+    returnError('Unauthorized access or invalid permission level', 401);
 }
 
 // Define the API endpoint to insert data
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Get the data from the request body
-    $data = json_decode(file_get_contents('php://input'), true);
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    // Validate the data
+    $validationRules = [
+        "order_id" => [
+            "required" => true,
+             "string" => true
+            ],
+            "user_id" => ["required" => true, "string" => true],
+            "warehouse_id" => ["required" => true, "string" => true],
+            "rider_id" => ["required" => true, "string" => true],
+            "fridge_id" => ["required" => true, "string" => true],
+            "operator_id" => ["required" => true, "string" => true],
+            "order_date" => ["required" => true, "date_format" => "Y-m-d H:i:s"],
+            "delivery_address" => ["required" => true, "string" => true],
+            "delivery_instructions" => ["string" => true],
+            "payment_method" => ["required" => true, "string" => true],
+            "payment_status" => ["required" => true, "string" => true],
+            "order_status" => ["required" => true, "string" => true],
+            "total_amount" => ["required" => true, "string" => true],
+            "delivery_fee" => ["required" => true, "string" => true],
+            "delivery_time_estimate" => ["required" => true, "string" => true],
+            "actual_delivery_time" => ["required" => true, "date_format" => "Y-m-d H:i:s"],
+            "order_items" => ["required" => true, "array" => true],
+            "promotion_id" => ["string" => true],
+            "discount_amount" => ["string" => true],
+            "customer_rating" => ["string" => true],
+            "refund_status" => ["string" => true],
+            "refund_reason" => ["string" => true],
+            "order_edit_history" => ["array" => true],
+            "pickup_time" => ["required" => true, "date_format" => "Y-m-d H:i:s"],
+            "drop_time" => ["required" => true, "date_format" => "Y-m-d H:i:s"],
+            "delivery_distance" => ["string" => true],
+    ];
+
+    // Validate the data
+    $errors = validateData($requestData, $validationRules);
+
+    // Check if there are validation errors
+    if (!empty($errors)) {
+        returnError('Invalid input data', $errors);
+        exit();
+    }
 
     // Select the database and collection to insert into
     $db = $mongoClient->selectDatabase("storage_data");
     $collection = $db->selectCollection("orders");
 
     // Insert the document into the collection
-    $result = $collection->insertOne($data);
+    $result = $collection->insertOne($requestData);
 
     // Check if the insertion was successful and send response accordingly
     if ($result->getInsertedCount() == 1) {
-        $response = array("success" => true, "message" => "Document inserted successfully.");
+        returnResponse('Document inserted successfully');
     } else {
-        $response = array("success" => false, "message" => "Failed to insert document. Error: " . $result->getWriteErrors()[0]["errmsg"]);
+        returnError('Failed to insert document', 500);
+    }
+}
+
+// Data is valid, continue processing
+
+/**
+ * Validate the data against the provided rules.
+ *
+ * @param array $data The data to be validated.
+ * @param array $rules The validation rules.
+ * @return array The validation errors, if any.
+ */
+function validateDateFormat($dateString, $format)
+{
+    $dateTime = DateTime::createFromFormat($format, $dateString);
+    return $dateTime && $dateTime->format($format) === $dateString;
+}
+
+function validateData($data, $rules)
+{
+    $errors = [];
+
+    foreach ($rules as $field => $fieldRules) {
+        foreach ($fieldRules as $rule => $value) {
+            switch ($rule) {
+                case 'required':
+                    if ($value && !isset($data[$field])) {
+                        $errors[$field][] = 'The field is required.';
+                    }
+                    break;
+                case 'integer':
+                    if (isset($data[$field]) && !is_numeric($data[$field])) {
+                        $errors[$field][] = 'The field must be an integer.';
+                    }
+                    break;
+                case 'numeric':
+                    if (isset($data[$field]) && !is_numeric($data[$field])) {
+                        $errors[$field][] = 'The field must be numeric.';
+                    }
+                    break;
+                case 'string':
+                    if (isset($data[$field]) && !is_string($data[$field])) {
+                        $errors[$field][] = 'The field must be a string.';
+                    }
+                    break;
+                case 'max_length':
+                    if (isset($data[$field]) && strlen($data[$field]) > $value) {
+                        $errors[$field][] = 'The field length exceeds the maximum allowed length.';
+                    }
+                    break;
+                case 'min':
+                    if (isset($data[$field]) && $data[$field] < $value) {
+                        $errors[$field][] = 'The field value is below the minimum allowed value.';
+                    }
+                    break;
+                case 'in':
+                    if (isset($data[$field]) && !in_array($data[$field], $value)) {
+                        $errors[$field][] = 'The field value is not allowed.';
+                    }
+                    break;
+                case 'date_format':
+                    if (isset($data[$field]) && !validateDateFormat($data[$field], $value)) {
+                        $errors[$field][] = 'The field does not match the required date format.';
+                    }
+                    break;
+                case 'array':
+                    if (isset($data[$field]) && !is_array($data[$field])) {
+                        $errors[$field][] = 'The field must be an array.';
+                    }
+                    break;
+                case 'nullable':
+                    if (isset($data[$field]) && is_null($data[$field])) {
+                        unset($data[$field]);
+                    }
+                    break;
+                // Add validation rules for other data types if needed
+            }
+        }
     }
 
-    // Send the response back to the client
+    return $errors;
+}
+
+/**
+ * Return an error response with the specified message and HTTP status code.
+ *
+ * @param string $message The error message.
+ * @param int $statusCode The HTTP status code for the error response.
+ */
+function returnError($message, $statusCode)
+{
+    $response = [
+        'error' => $message,
+    ];
+
+    http_response_code($statusCode);
+
     echo json_encode($response);
+    exit();
+}
+
+/**
+ * Return a success response with the specified message.
+ *
+ * @param string $message The success message.
+ */
+function returnResponse($message)
+{
+    $response = [
+        'message' => $message,
+    ];
+
+    http_response_code(200);
+
+    echo json_encode($response);
+    exit();
 }
 ?>
